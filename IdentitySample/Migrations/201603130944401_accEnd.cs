86de8ac36a99d9db4d1504d48793682bf04e0b47
mdonev <marjandonev@outlook.com>
2016-03-13 15:31:19 +0100

    template
    pocheten template

---
+++ IdentitySample/Migrations/201603130944401_accEnd.cs
@@ -0,0 +1,16 @@
+namespace IdentitySample.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class accEnd : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ IdentitySample/Models/IdentityModels.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace IdentitySample.Models
+{
+    public class ApplicationUserLogin : IdentityUserLogin<int> { }
+    public class ApplicationUserClaim : IdentityUserClaim<int> { }
+    public class ApplicationUserRole : IdentityUserRole<int> { }
+
+    public class ApplicationRole : IdentityRole<int, ApplicationUserRole>, IRole<int>
+    {
+        public string Description { get; set; }
+
+        public ApplicationRole() : base() { }
+        public ApplicationRole(string name)
+            : this()
+        {
+            this.Name = name;
+        }
+
+        public ApplicationRole(string name, string description)
+            : this(name)
+        {
+            this.Description = description;
+        }
+    }
+
+
+    public class ApplicationUser : IdentityUser<int, ApplicationUserLogin, ApplicationUserRole, ApplicationUserClaim>, IUser<int>
+    {
+        public async Task<ClaimsIdentity>
+            GenerateUserIdentityAsync(UserManager<ApplicationUser, int> manager)
+        {
+            var userIdentity = await manager
+                .CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            return userIdentity;
+        }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+
+        // Use a sensible display name for views:
+        [Display(Name = "Postal Code")]
+        public string PostalCode { get; set; }
+
+        // Concatenate the address info for display in tables and such:
+        public string DisplayAddress
+        {
+            get
+            {
+                string dspAddress =
+                    string.IsNullOrWhiteSpace(this.Address) ? "" : this.Address;
+                string dspCity =
+                    string.IsNullOrWhiteSpace(this.City) ? "" : this.City;
+                string dspState =
+                    string.IsNullOrWhiteSpace(this.State) ? "" : this.State;
+                string dspPostalCode =
+                    string.IsNullOrWhiteSpace(this.PostalCode) ? "" : this.PostalCode;
+
+                return string
+                    .Format("{0} {1} {2} {3}", dspAddress, dspCity, dspState, dspPostalCode);
+            }
+        }
+    }
+
+
+    public class ApplicationDbContext
+        : IdentityDbContext<ApplicationUser, ApplicationRole, int,
+        ApplicationUserLogin, ApplicationUserRole, ApplicationUserClaim>
+    {
+        public ApplicationDbContext()
+            : base("AccountConnection")
+        {
+        }
+
+        static ApplicationDbContext()
+        {
+            Database.SetInitializer<ApplicationDbContext>(new ApplicationDbInitializer());
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+
+
+    public class ApplicationUserStore :
+    UserStore<ApplicationUser, ApplicationRole, int,
+    ApplicationUserLogin, ApplicationUserRole, ApplicationUserClaim>, IUserStore<ApplicationUser, int>, IDisposable
+    {
+        public ApplicationUserStore()
+            : this(new IdentityDbContext())
+        {
+            base.DisposeContext = true;
+        }
+
+        public ApplicationUserStore(DbContext context)
+            : base(context)
+        {
+        }
+    }
+
+
+    public class ApplicationRoleStore
+    : RoleStore<ApplicationRole, int, ApplicationUserRole>,
+    IQueryableRoleStore<ApplicationRole, int>,
+    IRoleStore<ApplicationRole, int>, IDisposable
+    {
+        public ApplicationRoleStore()
+            : base(new IdentityDbContext())
+        {
+            base.DisposeContext = true;
+        }
+
+        public ApplicationRoleStore(DbContext context)
+            : base(context)
+        {
+        }
+    }
+}
